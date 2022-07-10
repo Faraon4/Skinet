e34940e81ec09ee4721b06f2291d1e044a3fb696
Faraon4 <radu.darii130399@mail.ru>
2022-07-11 01:48:40 +0200

    create the api to work correct with the 500 server error

---
+++ API/Controllers/ErrorController.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Errors;
+using Microsoft.AspNetCore.Mvc; // we need this for Route("")
+
+namespace API.Controllers
+{
+    // We create this controller, that can generate our response (in our case ApiResponse())
+    [Route("errors/{code}")]
+    public class ErrorController : BaseApiController
+    {
+        public IActionResult Error(int code)
+        {
+            return new ObjectResult(new ApiResponse(code));
+        }
+    }
+}
