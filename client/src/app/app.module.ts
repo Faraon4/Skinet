7665c1b4b166ecf325f2f951d38913333852c84f
Faraon4 <radu.darii130399@mail.ru>
2022-10-05 01:48:50 +0200

    Begin section 9 : Create folder structure: Core: for Singelton in our app -> example nav-bar Shared: staff that is shared among application Shop: We create this module , and all childs that we will add, we will add in this folder, (everything related to the shop), and routing as well

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,18 +5,18 @@ import {HttpClientModule} from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavBarComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    CoreModule    //  Instead of putting here all modules that we create, we just put a file where we put all modules
   ],
   providers: [],
   bootstrap: [AppComponent]
