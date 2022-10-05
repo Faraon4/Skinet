927dad1b60b0b58cc9b6d1b94e7abaf599cc5a09
Faraon4 <radu.darii130399@mail.ru>
2022-10-05 02:03:31 +0200

    Work with the service and do some changes in the files and in the file names

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
+import { ShopModule } from './shop/shop.module';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { CoreModule } from './core/core.module';
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    CoreModule    //  Instead of putting here all modules that we create, we just put a file where we put all modules
+    CoreModule,    //  Instead of putting here all modules that we create, we just put a file where we put all modules
+    ShopModule, // Module created by us, such way , HTML sees that we have this module and will have the necesary information
   ],
   providers: [],
   bootstrap: [AppComponent]
