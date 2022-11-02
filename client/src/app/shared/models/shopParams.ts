5eeb64fce14098b2464a872ed3fa1a1cead3398a
Faraon4 <radu.darii130399@mail.ru>
2022-11-02 23:35:42 +0100

    Pagination Part 1: Just make it to look as it should with the information gained from the server

---
+++ client/src/app/shared/models/shopParams.ts
@@ -0,0 +1,8 @@
+export class ShopParams {
+    brandId: number = 0; // We add = 0 to select from the beginning to be ALL brand selected
+ typeId: number = 0; // We add = 0 to select from the beginning to be ALL type selected
+ sort = 'name'
+ 
+ pageNumber = 1
+ pageSize = 6
+}
--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { IBrand } from '../shared/models/brand';
 import { IProduct } from '../shared/models/product';
 import { IType } from '../shared/models/productType';
+import { ShopParams } from '../shared/models/shopParams';
 import { ShopService } from './shop.service';
 
 @Component({
@@ -13,10 +14,9 @@ export class ShopComponent implements OnInit {
  products: IProduct[];
  brands: IBrand[];
  types: IType[];
- brandIdSelected: number = 0; // We add = 0 to select from the beginning to be ALL brand selected
- typeIdSelected: number = 0; // We add = 0 to select from the beginning to be ALL type selected
+ shopParams = new ShopParams();
 
- sortSelected = 'name'
+ totalCount: number
 
  // Speling is very important, because only such way we get information from our backend
  sortOptions = [
@@ -37,8 +37,11 @@ export class ShopComponent implements OnInit {
 
 
   getProducts(){
-    this.shopService.getProducts(this.brandIdSelected, this.typeIdSelected, this.sortSelected).subscribe(response => {
+    this.shopService.getProducts(this.shopParams).subscribe(response => {
       this.products = response.data;
+      this.shopParams.pageNumber = response.pageIndex;
+      this.shopParams.pageSize = response.pageSize;
+      this.totalCount = response.count;
     }, error => {
       console.log(error)
     })
@@ -66,20 +69,20 @@ export class ShopComponent implements OnInit {
 
 
   onBrandSelected(brandId: number){
-    this.brandIdSelected = brandId;
+    this.shopParams.brandId = brandId;
     this.getProducts();
 
   }
 
 
   onTypeSelected(typeId: number){
-    this.typeIdSelected = typeId;
+    this.shopParams.typeId = typeId;
     this.getProducts();
   }
 
 
 onSortSelected(sort: string){
-  this.sortSelected = sort;
+  this.shopParams.sort = sort;
   this.getProducts();
 }
 
